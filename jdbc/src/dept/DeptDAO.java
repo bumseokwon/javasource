e3a2446b2ea30d139f6b3e55e88aba541a6cf1ae
wonbumseok <qjatjrsla1@naver.com>
2021-09-30 16:16:37 +0900

    database 커넥션

--- jdbc/src/dept/DeptDAO.java
+++ jdbc/src/dept/DeptDAO.java
@@ -1,5 +1,16 @@
 package dept;
 
 public class DeptDAO {
-
-}
+       //empDAO stafic, Connection
+	static {
+		try {
+			Class.forName("oracle. jdbc.OracleDriver");
+			
+		} catch (ClassNotFoundException e)  {
+			e.printStackTrace();
+		}
+	
+    }
+	public static Connection getConnection() {
+		String url = "jdbc:oracle:thin:@local"
+	}
